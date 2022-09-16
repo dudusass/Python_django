c82464ba9d43956847711d520cbb9ed86a535c73
Sass <dudu_sass12@hotmail.com>
2022-09-16 19:57:46 -0300

    feat: get filter

--- taskboard/views.py
+++ taskboard/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Board, Task
 from .serializer import BoardSerializer, TaskSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
@@ -12,13 +14,8 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer    
 
-
-""" def index(request):
-    return render(request, "index.html")
-
-def carrinho(request):
-    return render(request, "carrinho.html")    
-
-def contato(request):
-    return render(request, "contato.html")      
- """
+    @action(detail=False)
+    def pending(self, request):
+        queryset = Task.objects.all().filter(status="Pending")
+        serializer = TaskSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
